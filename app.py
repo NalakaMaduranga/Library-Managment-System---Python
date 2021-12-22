4dc1b87df01d8499405bbf874228af69666cbdeb
Nalaka Maduranga <nalakathushan123@gmail.com>
2021-12-22 12:14:38 +0530

    User input

---
+++ app.py
@@ -0,0 +1,9 @@
+option = input()
+while option == 'x' and option =="x":
+    #do stuff here
+    if option == '1':
+        print("You choose 1")
+    elif option == '2':
+        print("you choose 2")
+    option = input()
+        
