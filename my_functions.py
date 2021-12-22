cf58e967328a344efb049b58a3302ba306218790
Nalaka Maduranga <nalakathushan123@gmail.com>
2021-12-22 12:47:06 +0530

    function operation file

---
+++ my_functions.py
@@ -0,0 +1,11 @@
+#print options
+def print_options():
+    print("Press the specific Button")
+    print("1- Create a new book")
+    print("2-Save book locally")
+    print("3-load book from disk")
+    print("4-issue book")
+    print("5-return a book")
+    print("6-update a book")
+    print("7-show all book")
+    print("8-Find book by id")
