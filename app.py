f9e3c3074427aa3e5f269aff1da905fb4e0af93c
Nalaka Maduranga <nalakathushan123@gmail.com>
2021-12-25 18:14:39 +0530

    update for load book function

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ while option != 'X' and option !="x":
     #do stuff here
     if option == '1':
         books.append(my_functions.create_book()) 
-        input("COMMAND EXECUTED..PRES ANY BUTTON TO CONTINUE")
+      
     elif option == '2':
         my_functions.save_book(books)
+    elif option =='3':
+        books=my_functions.load_books()
+    else:
+        print("The given command not exists")
+    input("press enter to continue")
     #asking for input again
     os.system("cls")
     my_functions.print_options()
