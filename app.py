3334f3cfdda1832d96b016cf15e9fb082a7e57e5
Nalaka Maduranga <nalakathushan123@gmail.com>
2021-12-25 16:03:20 +0530

    Create book function implemented

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import my_functions  #import module that created
+import os
+my_functions.print_options()
 option = input()
+books=[]
 while option != 'X' and option !="x":
     #do stuff here
     if option == '1':
-        print("You choose 1")
+        books.append(my_functions.create_book()) 
+        input("COMMAND EXECUTED..PRES ANY BUTTON TO CONTINUE")
     elif option == '2':
         print("you choose 2")
+    #asking for input again
+    os.system("cls")
+    my_functions.print_options()
     option = input()
         
--- my_functions.py
+++ my_functions.py
@@ -1,3 +1,4 @@
+from book import Book
 #print options
 def print_options():
     print("Press the specific Button")
@@ -9,3 +10,36 @@ def print_options():
     print("6-update a book")
     print("7-show all book")
     print("8-Find book by id")
+
+def input_book_info():
+    id=input("ID: ")
+    name=input("Name: ")
+    descdription =input("Description: ")
+    isbn = input("ISBN: ")
+    page_count = int(input("Page count: "))
+    issued = input("Issued: y/Y")
+    issued = (issued =="y" or issued == "Y")
+    author = input("Author Name")
+    year = int(input("Year: "))
+    return{
+        'id': id,
+        'name':name,
+        'description':descdription,
+        'isbn':isbn,
+        'page_count':page_count,
+        'issued':issued,
+        'author':author,
+        'year':year
+    }
+
+    
+
+#create book function
+def create_book():
+    print("Please ENter your information")
+    book_input = input_book_info()
+    book = Book(book_input['id'],book_input['name'],book_input['description'],book_input['isbn'],book_input['page_count'],
+    book_input['issued'],book_input['author'],book_input['year'])
+    print(book.to_dict())
+    return book 
+
